408251c3ede195b525df47713a6e6ac44445f8ac
Jake Sijing Lin <jakesijinglin@Jakes-Mac.local>
2018-08-29 10:47:10 -0500

    download image from aws and recognize image

---
+++ ARMagazine/ViewController.swift
@@ -0,0 +1,121 @@
+import UIKit
+import SpriteKit
+import ARKit
+
+struct ImageInformation {
+    let name: String
+    let description: String
+    let image: UIImage
+}
+
+class ViewController: UIViewController, ARSKViewDelegate {
+    @IBOutlet var sceneView: ARSKView!
+    var selectedImage : ImageInformation?
+    
+    var images : [String? : ImageInformation] = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        
+        if let scene = SKScene(fileNamed: "Scene") {
+            sceneView.presentScene(scene)
+        }
+        
+        let url = URL(string: "https://s3.amazonaws.com/netc-http/AR/NET.jpg")
+        let task = URLSession.shared.dataTask(with: url!){ (data, response, error) in
+            
+            if (error != nil){
+                print("Error")
+            }
+            else{
+                var documentsDirectory:String?
+                
+                var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+                
+                if paths.count > 0
+                {
+                    documentsDirectory = paths[0]
+                    let savePath = documentsDirectory! + "/NET.jpg"
+                    FileManager.default.createFile(atPath: savePath, contents: data, attributes: nil)
+                    
+                    DispatchQueue.main.async{
+                        guard let uiimage = UIImage(named: savePath),
+                        let ciimage = CIImage(image: uiimage),
+                            let cgimage = self.convertCIImageToCGImage(inputImage: ciimage)else { return }
+                        
+                        let arImage = ARReferenceImage(cgimage, orientation: CGImagePropertyOrientation.up, physicalWidth: 0.2)
+                        
+                        arImage.name = "CGImage Test"
+                        let configuration = ARWorldTrackingConfiguration()
+                        configuration.detectionImages = [arImage]
+                        
+                        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+                        
+                        self.images = [arImage.name : ImageInformation(name: "This is a title", description: "This is a description", image: uiimage)]
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+        
+        /*guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "ImageReference", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
+        
+        let referenceImages = arImage
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        */
+    }
+    
+    
+    // MARK: - ARSKViewDelegate
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        
+        if let imageAnchor = anchor as? ARImageAnchor,
+            let referenceImageName = imageAnchor.referenceImage.name,
+            let scannedImage =  self.images[referenceImageName] {
+            
+            self.selectedImage = scannedImage
+            
+            self.performSegue(withIdentifier: "showImageInformation", sender: self)
+            
+            return imageSeenMarker()
+        }
+        
+        return nil
+    }
+    
+    private func imageSeenMarker() -> SKLabelNode {
+        let labelNode = SKLabelNode(text: "✅")
+        labelNode.horizontalAlignmentMode = .center
+        labelNode.verticalAlignmentMode = .center
+        
+        return labelNode
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showImageInformation"{
+            if let imageInformationVC = segue.destination as? ImageInformationViewController,
+                let actualSelectedImage = selectedImage {
+                imageInformationVC.imageInformation = actualSelectedImage
+            }
+        }
+    }
+    
+    func convertCIImageToCGImage(inputImage: CIImage) -> CGImage? {
+        let context = CIContext(options: nil)
+        if let cgImage = context.createCGImage(inputImage, from: inputImage.extent) {
+            return cgImage
+        }
+        return nil
+    }
+}
